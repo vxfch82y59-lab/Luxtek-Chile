4ba3b7d3533dcb5f4ad57d9e3e8c23ef313a6325
Ethem <vxfch82y59@privaterelay.appleid.com>
2025-11-27 14:27:07 -0300

    Delete lang.js

--- lang.js
+++
@@ -1,9 +0,0 @@
-function setLang(lang) {
-        document.querySelectorAll("[data-" + lang + "]").forEach(el => {
-                el.innerText = el.getAttribute("data-" + lang);
-                    });
-                    }
-
-                    // WhatsApp numarasını daha sonra ekleyeceğiz
-                    document.getElementById("wp").href = "https://wa.me/XXXXXXXXXX";
-}
