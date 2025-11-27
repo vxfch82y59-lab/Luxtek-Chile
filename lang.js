0105bedc2aa4273387c61e7255869ac6c779af70
vxfch82y59-lab <vxfch82y59@privaterelay.appleid.com>
2025-11-27 11:34:12 -0300

    Delete lang.js

--- lang.js
+++
@@ -1,8 +0,0 @@
-function setLang(lang) {
-    document.querySelectorAll("[data-" + lang + "]").forEach(el => {
-        el.innerText = el.getAttribute("data-" + lang);
-    });
-}
-
-// WhatsApp numarasını sonra değiştir
-document.getElementById("wp").href = "https://wa.me/XXXXXXXXXX";
