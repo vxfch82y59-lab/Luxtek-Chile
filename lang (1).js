b6e0f1a9b6ae8698b8094dee0799e475cec6921e
Ethem <vxfch82y59@privaterelay.appleid.com>
2025-11-27 14:37:16 -0300

    Add files via upload

---
+++ lang (1).js
@@ -0,0 +1,8 @@
+function setLang(lang) {
+        document.querySelectorAll("[data-" + lang + "]").forEach(el => {
+                el.innerText = el.getAttribute("data-" + lang);
+                    });
+                    }
+
+                    document.getElementById("wp").href = "https://wa.me/XXXXXXXXXX";
+}
